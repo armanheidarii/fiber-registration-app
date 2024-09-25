bee42a5c1a2945d666dd843575fd9a0b49ccaac5
armanheids <armanheids@gmail.com>
2024-09-25 04:07:03 +0330

    middlewares added

---
+++ middlewares/auth.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"os"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func JWTMiddleware(c *fiber.Ctx) error {
+	tokenString := c.Get("Authorization")
+	tokenString = strings.TrimPrefix(tokenString, "Bearer ")
+
+	claims := jwt.MapClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+
+	if err != nil || !token.Valid {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{"error": "Unauthorized"})
+	}
+
+	if username, ok := claims["username"].(string); ok {
+		c.Locals("username", username)
+	}
+
+	return c.Next()
+}
